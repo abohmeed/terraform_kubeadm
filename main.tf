4878c703479832b039e9bd93b4fe4a5b3e96eddf
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-11-12 18:31:54 +0100

    Changes the default region

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "eu-west-1"
+  region  = "eu-west-2"
 }
 terraform {
    backend "s3" {
@@ -10,14 +10,14 @@ terraform {
 }
 module "kubernetes" {
   source = "./kubernetes"
-  ami = "ami-029a72555b2a91206" # PLEASE REMEMBER TO CHANGE THIS AMI TO YOUR OWN
-  cluster_name = "skyvalley"
+  ami = "ami-00da2ce1db1f11a7d"
+  cluster_name = "basic-cluster"
   master_instance_type = "t3.medium"
   nodes_max_size = 1
   nodes_min_size = 1
   private_subnet01_netnum = "1"
   public_subnet01_netnum = "2"
-  region = "eu-west-1"
+  region = "eu-west-2"
   vpc_cidr_block = "10.240.0.0/16"
   worker_instance_type = "t3.medium"
   vpc_name = "kubernetes"
