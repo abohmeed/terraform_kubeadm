e68715376da1c9be8d6b1e0a3c89740c86288b95
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-11-14 16:28:13 +0100

    Some prettification

--- main.tf
+++ main.tf
@@ -3,24 +3,15 @@ provider "aws" {
 }
 terraform {
    backend "s3" {
-<<<<<<< HEAD
-    bucket = # Add your S3 bucket here
-=======
-    bucket = # Your bucket
->>>>>>> e914bf3781c8444250ed99fa80aded092e6cafc6
+    bucket = "# Add your S3 bucket here"
     key    = "default.tfstate"
-    region = # Add the S3 bucket region
+    region = "# Add the S3 bucket region"
   }
 }
 module "kubernetes" {
   source = "./kubernetes"
-<<<<<<< HEAD
-  ami = "ami-0d47c6174ad79eb43"
-  cluster_name = "basic-cluster"
-=======
-  ami = # Your configured AMI
-  cluster_name = # Your cluster name
->>>>>>> e914bf3781c8444250ed99fa80aded092e6cafc6
+  ami = "# Your configured AMI"
+  cluster_name = "# Your cluster name"
   master_instance_type = "t3.medium"
   nodes_max_size = 1
   nodes_min_size = 1
@@ -30,9 +21,5 @@ module "kubernetes" {
   vpc_cidr_block = "10.240.0.0/16"
   worker_instance_type = "t3.medium"
   vpc_name = "kubernetes"
-<<<<<<< HEAD
-  ssh_public_key = # Add your public SSH key
-=======
-  ssh_public_key = # You SSH public key
->>>>>>> e914bf3781c8444250ed99fa80aded092e6cafc6
+  ssh_public_key = "# Add your public SSH key"
 }
